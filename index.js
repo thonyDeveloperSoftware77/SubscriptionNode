cbe16555b56d442d94d3ba4ec9d53bdd9a9edb0e
Anthony Cochea <thonydevelopersoftware@gmail.com>
2023-11-20 13:48:03 -0500

    Comentarios

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+/*Funcion que crea las instancias*/
 function crearComponente(id, correo) {
     var client = mqtt.connect('mqtt://broker.hivemq.com')
 
+    /*Metodo para que el componente se suscriba a un topico*/
     client.on('connect', function () {
         client.subscribe('correos_a_enviar', function (err) {
             if (!err) {
@@ -19,17 +21,21 @@ function crearComponente(id, correo) {
         })
     })
 
+    /*El mensaje ejecuta la funcion enviar correo en el caso que exista alguna publicacion suscrita al topico*/
     client.on('message', function (topic, message) {
         console.log('\nComponente ' + id + ' recibió en el tópico ' + topic + ': ' + message.toString())
         enviarCorreo(correo, message.toString(), id * 1000)
     })
 }
 
+
+/*Funcion que recibe como input el mensaje por consola*/
 function pedirMensaje(client) {
     rl.question('Introduce el mensaje que quieres enviar (o "salir" para terminar): ', (mensaje) => {
         if (mensaje.toLowerCase() === 'salir') {
             rl.close();
         } else {
+            /*Se publica el mensaje al topico*/
             client.publish('correos_a_enviar', mensaje);
             pedirMensaje(client);
         }
